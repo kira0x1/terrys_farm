6e5f2e5edf6d16706728dd2af4a5267fc6bfe9c4
Kira <kira.eva@protonmail.com>
2024-05-06 16:44:35 +0100

    Implemented harvesting

--- code/PlotSlot.cs
+++ code/PlotSlot.cs
@@ -16,8 +16,13 @@ public class PlotSlot
     public int TickCount { get; set; }
 
     public bool HasBloomed { get; set; }
+
     private TimeSince TimeSinceTick { get; set; }
-    private TimeSince BloomTextTime { get; set; }
+    private TimeSince BloomTextTime { get; set; } = 6;
+    private TimeSince HarvestTextTime { get; set; } = 6;
+
+    private const float harvestDelay = 4f;
+    private TimeSince harvestTime = 0;
 
 
     public PlotSlot(int x, int y, Vector3 position = new Vector3())
@@ -36,8 +41,16 @@ public class PlotSlot
 
     public void Tick()
     {
-        if (!IsOccupied || Category != CategoryTypes.Seeds)
+        if (Category != CategoryTypes.Seeds)
             return;
+
+
+        if (HarvestTextTime < 2)
+        {
+            Gizmo.Draw.Text("Harvested!!!", new Transform(position));
+        }
+
+        if (!IsOccupied) return;
 
         if (TimeSinceTick > Item.TickRate)
         {
@@ -45,9 +58,10 @@ public class PlotSlot
             TimeSinceTick = 0f;
         }
 
+        // On Bloom
         if (!HasBloomed && TickCount > Item.BloomTicks)
         {
-            BloomTextTime = 0;
+            OnBloom();
         }
 
         if (BloomTextTime < 2)
@@ -55,6 +69,29 @@ public class PlotSlot
             Gizmo.Draw.Text("Bloomed!!", new Transform(position));
         }
 
+
         HasBloomed = TickCount > Item.BloomTicks;
+
+        if (HasBloomed && harvestTime > harvestDelay)
+        {
+            Harvest();
+        }
+    }
+
+    private void OnBloom()
+    {
+        BloomTextTime = 0;
+        harvestTime = 0;
+    }
+
+    private void Harvest()
+    {
+        HarvestTextTime = 0;
+        TickCount = 0;
+        IsOccupied = false;
+        HasBloomed = false;
+
+        BloomTextTime = 10;
+        harvestTime = 10;
     }
 }
