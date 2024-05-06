0a6d7259d2dc69dd16d3af1c16f84c2609d63563
Kira <kira.eva@protonmail.com>
2024-05-06 14:58:09 +0100

    Display bloom text

--- code/PlotSlot.cs
+++ code/PlotSlot.cs
@@ -15,8 +15,9 @@ public class PlotSlot
     public int TickCount { get; set; }
 
     public bool HasBloomed { get; set; }
-
     private TimeSince TimeSinceTick { get; set; }
+
+    private TimeSince BloomTextTime { get; set; }
 
     public PlotSlot(int x, int y, Vector3 position = new Vector3())
     {
@@ -31,6 +32,16 @@ public class PlotSlot
         {
             TickCount++;
             TimeSinceTick = 0f;
+        }
+
+        if (!HasBloomed && TickCount > Item.BloomTicks)
+        {
+            BloomTextTime = 0;
+        }
+
+        if (BloomTextTime < 2)
+        {
+            Gizmo.Draw.Text("Bloomed!!", new Transform(position));
         }
 
         HasBloomed = TickCount > Item.BloomTicks;
